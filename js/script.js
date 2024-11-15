555dea63793db6da8e5a335c21c689e07cbde12d
frakibuddin <frakibuddin360@gmail.com>
2024-11-15 11:06:27 +0600

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let search_close = document.querySelector(".search-close");
 let header_search_bar = document.querySelector(".header-search-bar");
 let menu = document.querySelector("menu");
 let menu_tgl_btn = document.querySelector(".menu-tgl-btn");
+let eye_icon = document.querySelector(".eye-icon");
+let input_password = document.querySelector(".input-password");
 
 header_search.onclick = () => {
   header_search_bar.classList.add("search-open");
@@ -12,4 +14,14 @@ search_close.onclick = () => {
 };
 menu_tgl_btn.onclick = () => {
   menu.classList.toggle("show");
+};
+
+eye_icon.onclick = () => {
+  if (input_password.type == "password") {
+    input_password.type = "text";
+    eye_icon.classList.add("hide");
+  } else {
+    input_password.type = "password";
+    eye_icon.classList.remove("hide");
+  }
 };
