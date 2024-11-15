80bb2100d1436ba3b26a9414481b052e84e05ce2
frakibuddin <frakibuddin360@gmail.com>
2024-11-15 13:10:46 +0600

    update

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let search_close = document.querySelector(".search-close");
 let header_search_bar = document.querySelector(".header-search-bar");
 let menu = document.querySelector("menu");
 let menu_tgl_btn = document.querySelector(".menu-tgl-btn");
-let eye_icon = document.querySelector(".eye-icon");
-let input_password = document.querySelector(".input-password");
+let eye_icon = document.querySelectorAll(".eye-icon");
+let input_password = document.querySelectorAll(".input-password");
 
 header_search.onclick = () => {
   header_search_bar.classList.add("search-open");
@@ -15,13 +15,14 @@ search_close.onclick = () => {
 menu_tgl_btn.onclick = () => {
   menu.classList.toggle("show");
 };
-
-eye_icon.onclick = () => {
-  if (input_password.type == "password") {
-    input_password.type = "text";
-    eye_icon.classList.add("hide");
-  } else {
-    input_password.type = "password";
-    eye_icon.classList.remove("hide");
-  }
-};
+eye_icon.forEach((e, n) => {
+  e.onclick = () => {
+    if (input_password[n].type == "password") {
+      input_password[n].type = "text";
+      e.classList.add("hide");
+    } else {
+      input_password[n].type = "password";
+      e.classList.remove("hide");
+    }
+  };
+});
